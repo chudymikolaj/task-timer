933b8794a561cc8ccbe1cfc3bb4a90a97c151a56
chudymikolaj <chudymikolaj06@gmail.com>
2020-04-02 23:36:17 +0200

    Added Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import TaskTable from './components/TasksTable';
+
 function App() {
   return (
     <div className="App">
-     Hello
+     <TaskTable />
     </div>
   );
 }
